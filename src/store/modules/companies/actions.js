c5d6a1bb6e260541656a0172f10a886263c61105
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-08-23 22:06:26 -0300

    modulo 4 aula 03

--- src/store/modules/companies/actions.js
+++ src/store/modules/companies/actions.js
@@ -1,5 +1,13 @@
+import axios from 'axios'
+import { API_VERSION } from '../../../configs/api'
+
+const RESOURCE = '/tenants'
+
 const actions = {
-    
+    getCompanies ({commit}){
+        return axios.get(`${API_VERSION}${RESOURCE}`)
+                        .then(response => { commit('SET_COMPANY', response.data) })
+    }
 }
 
 export default actions
