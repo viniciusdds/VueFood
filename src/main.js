e23685f437d2245ddfee13e03f4d64747910f07b
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-08-29 22:03:21 -0300

    modulo 4 finalizado

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 require('./bootstrap')
+require('./plugins')
 import Vue from 'vue'
 import BaseTemplate from './layouts/BaseTemplate'
 import router from './routes'
 import store from './store'
 
 Vue.config.productionTip = false
+
+/** 
+ * Componentes Globais
+*/
+Vue.component('preloader-component', () => import('./components/Preloader'))
 
 new Vue({
   render: h => h(BaseTemplate),
--- src/store/modules/companies/actions.js
+++ src/store/modules/companies/actions.js
@@ -4,9 +4,13 @@ import { API_VERSION } from '../../../configs/api'
 const RESOURCE = '/tenants'
 
 const actions = {
-    getCompanies ({commit}){
+    getCompanies ({ commit }){
+        commit('SET_PRELOADER', true)
+        commit('SET_TEXT_PRELOADER', 'Carregando as Empresas')
+
         return axios.get(`${API_VERSION}${RESOURCE}`)
-                        .then(response => { commit('SET_COMPANY', response.data) })
+                        .then(response => commit('SET_COMPANY', response.data))
+                        .finally(() => commit('SET_PRELOADER', false))
     }
 }
 
